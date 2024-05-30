ecb163a69e348dbd20eacc94b40736d23ef1269e
AmanRaza78 <razakhanaman9@gmail.com>
2024-05-31 04:29:13 +0530

    authentication done

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,42 @@
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Navbar(){
-    return(
-        <nav className="border-b bg-background h-[10vh] flex item-center">
-            <div className="container flex items-center justify-between">
-                <Link href='/'>
-                    <h1 className="font-bold text-primary text-3xl">Buzz Board</h1>
-                </Link>
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
+  return (
+    <nav className="border-b bg-background h-[10vh] flex item-center">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <h1 className="font-bold text-primary text-3xl">Buzz Board</h1>
+        </Link>
 
-                <div className="flex items-center gap-x-5">
-                    <ModeToggle/>
+        <div className="flex items-center gap-x-5">
+          <ModeToggle />
 
-                    <div className="flex items-center gap-x-5">
-                        <Button>Login</Button>
-                        <Button>Sign Up</Button>
-                    </div>
-                </div>
+          {(await isAuthenticated()) ? (
+            <LogoutLink>
+              <Button>Logout</Button>
+            </LogoutLink>
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Login</Button>
+              </LoginLink>
+
+              <RegisterLink>
+                <Button>Sign Up</Button>
+              </RegisterLink>
             </div>
-        </nav>
-    )
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
