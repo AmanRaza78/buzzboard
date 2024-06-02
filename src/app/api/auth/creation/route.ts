cd065863fa58b80241cb4bbcc89e9677991dbb33
AmanRaza78 <razakhanaman9@gmail.com>
2024-06-02 18:08:20 +0530

    fixed redirecting

--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -29,5 +29,5 @@ export async function GET(){
         })
     }
 
-    return NextResponse.redirect("http://localhost:3000/")
+    return NextResponse.redirect(process.env.NODE_ENV==="development" ? "http://localhost:3000/": "https://buzzboard.vercel.app/")
 }
